e70f3fc270f2547f726d410d3b7dbeb01d9d9b1c
cykim <cykim8811@snu.ac.kr>
2024-08-08 20:31:56 +0000

    update c simulator

--- sim.py
+++ sim.py
@@ -20,10 +20,13 @@ class Memory:
     pointer_size = 4
     def __init__(self):
         self.memory = {}
+        self.functions = {}
     
     def get(self, addr: int, size: int):
         offset = addr % 2**16
         src = addr - offset
+        # print(hex(src), hex(offset), size)
+        # print(self.memory[src])
         if src in self.memory:
             return self.memory[src][offset:offset+size]
         raise KeyError("Segmentation fault")
@@ -85,12 +88,17 @@ class Context:
         self.memory = Memory() if parent is None else parent.memory
         self.vars: dict[str, Variable] = {}
     
-    def __getitem__(self, key: str):
+    def __getitem__(self, key: str|Address) -> Any:
+        if isinstance(key, Address):
+            print("HERE", key, key + 1, key.size)
+            return int.from_bytes(self.memory.get(key, key.size), 'little')
         if key in self.vars:
             var = self.vars[key]
             if '*' in var.vartype:
                 val = [int.from_bytes(self.memory.get(var.ptr+i, Memory.pointer_size), 'little') for i in range(var.count)]
-                return val if var.count > 1 else val[0]
+                val = [Address(v, Memory.pointer_size) for v in val]
+                if var.count > 1: return val
+                else: return val[0]
             elif var.vartype in ['int', 'long', 'short', 'char']:
                 size = self.get_type_size(var.vartype)
                 val = [int.from_bytes(self.memory.get(var.ptr+i, size), 'little', signed=True) for i in range(var.count)]
@@ -105,6 +113,8 @@ class Context:
             elif var.vartype == 'double':
                 val = [struct.unpack('d', self.memory.get(var.ptr+i, 8))[0] for i in range(var.count)]
                 return val if var.count > 1 else val[0]
+            elif var.vartype == 'function':
+                return self.memory.functions[var.ptr]
             else:
                 raise NotImplementedError(var.vartype)
         if self.parent is not None:
@@ -129,6 +139,8 @@ class Context:
                     self.memory.set(var.ptr, 4, struct.pack('f', value))
                 elif var.vartype == 'double':
                     self.memory.set(var.ptr, 8, struct.pack('d', value))
+                elif var.vartype == 'function':
+                    self.memory.functions[var.ptr] = value
                 else:
                     raise NotImplementedError(var.vartype)
             elif self.parent is not None:
@@ -143,23 +155,43 @@ class Context:
     def assign(self, key: str, vartype: str, count: int) -> Address:
         size = self.get_type_size(vartype) * count
         ptr = self.memory.malloc(size)
-        addr = Address(ptr, size)
+        if vartype == 'function':
+            self.memory.functions[ptr] = None
+        addr = Address(ptr, self.get_type_size(vartype))
         self.vars[key] = Variable(addr, vartype, count)
         return addr
 
     def get_type_size(self, vartype: str) -> int:
         if "*" in vartype:
             return Memory.pointer_size
+        if vartype == "function":
+            return 1
         return ctypes.sizeof(getattr(ctypes, 'c_' + vartype))
     
     def free(self, key: str):
         var = self.vars[key]
         self.memory.free(var.ptr)
         del self.vars[key]
+
+    def get_pointer(self, key: str) -> Address:
+        return Address(self.vars[key].ptr, Memory.pointer_size)
+
+    def get_from_pointer(self, key: str) -> Any:
+        addr = self[key]
+        size = self.get_type_size(self.vars[key].vartype)
+        return self[Address(addr, size)]
     
     def create_child(self) -> 'Context':
         return Context(self)
-
+    
+    def __str__(self):
+        # return str({
+        total = {}
+        target = self
+        while target is not None:
+            total.update({key: (target[key] if target.vars[key].vartype != 'function' else f'<function \'{key}>\' at {target.vars[key].ptr}') for key in target.vars})
+            target = target.parent
+        return str(total)
 
 
 class Executer:
@@ -208,29 +240,103 @@ class Executer:
         else:
             raise NotImplementedError(node.type.__class__.__name__)
 
+        return res
+
+    def execute_ArrayRef(self, node: c_ast.ArrayRef, context: Context, left=False):
+        if left:
+            addr = self.evaluate(node.name, context)
+            index = self.evaluate(node.subscript, context)
+            new_addr = addr + index
+            return new_addr
+        else:
+            addr = self.evaluate(node.name, context)
+            index = self.evaluate(node.subscript, context)
+            new_addr = addr + index
+            return context[new_addr]
+
     def execute_FuncDef(self, node: c_ast.FuncDef, context: Context):
-        print(context.vars)
-        print(context['s'])
-        print(context['k'])
-        print(context['asd'])
-        print(context['q'])
+        name = node.decl.name
+        context.assign(name, 'function', 1)
+        context[name] = node
     
     def execute_InitList(self, node: c_ast.InitList, context: Context):
         return [self.execute(n, context) for n in node.exprs]
 
-    def execute_UnaryOp(self, node: c_ast.UnaryOp, context: Context):
+    def execute_UnaryOp(self, node: c_ast.UnaryOp, context: Context, left=False):
+        if left and not node.op == '*':
+            raise SyntaxError("Invalid left operand")
+        if node.op == '&':
+            if isinstance(node.expr, c_ast.ID):
+                return context.get_pointer(node.expr.name)
+            else:
+                raise NotImplementedError(node.expr.__class__.__name__)
+        elif node.op == '*':
+            if isinstance(node.expr, c_ast.ID):
+                if left:
+                    return context[node.expr.name]
+                else:
+                    return context.get_from_pointer(node.expr.name)
+            else:
+                raise NotImplementedError(node.expr.__class__.__name__)
+        val = self.evaluate(node.expr, context)
         if node.op == '+':
-            return self.execute(node.expr, context)
+            return val
         elif node.op == '-':
-            return -self.execute(node.expr, context)
+            return -val
         elif node.op == '!':
-            return not self.execute(node.expr, context)
+            return not val
         elif node.op == '~':
-            return ~self.execute(node.expr, context)
-        elif node.op == '&':
-            return context.vars[self.execute(node.expr, context)].ptr
+            return ~val
         else:
             raise NotImplementedError(node.op)
+    
+    def execute_BinaryOp(self, node: c_ast.BinaryOp, context: Context):
+        left = self.evaluate(node.left, context)
+        right = self.evaluate(node.right, context)
+        if node.op == '+':
+            return left + right
+        elif node.op == '-':
+            return left - right
+        elif node.op == '*':
+            return left * right
+        elif node.op == '/':
+            return left / right
+        elif node.op == '%':
+            return left % right
+        elif node.op == '<<':
+            return left << right
+        elif node.op == '>>':
+            return left >> right
+        elif node.op == '&':
+            return left & right
+        elif node.op == '|':
+            return left | right
+        elif node.op == '^':
+            return left ^ right
+        elif node.op == '&&':
+            return left and right
+        elif node.op == '||':
+            return left or right
+        elif node.op == '==':
+            return left == right
+        elif node.op == '!=':
+            return left != right
+        elif node.op == '<':
+            return left < right
+        elif node.op == '>':
+            return left > right
+        elif node.op == '<=':
+            return left <= right
+        elif node.op == '>=':
+            return left >= right
+        else:
+            raise NotImplementedError(node.op)
+    
+    def execute_Compound(self, node: c_ast.Compound, context: Context):
+        for decl in node.block_items:
+            if isinstance(decl, c_ast.Return):
+                return self.execute(decl, context)
+            self.execute(decl, context)
     
     def execute_Constant(self, node: c_ast.Constant, context: Context):
         if node.type == 'int':
@@ -243,12 +349,46 @@ class Executer:
             return ord(node.value[1])
         else:
             raise NotImplementedError(node.type)
+    
+    def execute_Assignment(self, node: c_ast.Assignment, context: Context):
+        val = self.evaluate(node.rvalue, context)
+        if node.op == '=':
+            addr = self.evaluate(node.lvalue, context, left=True)
+            context[addr] = val
+        else:
+            raise NotImplementedError(node.op)
 
-    def execute(self, node, context: Context):
+        print(context)
+
+    def execute_FuncCall(self, node: c_ast.FuncCall, context: Context):
+        args = [self.execute(arg, context) for arg in node.args.exprs]
+        ftn = context[node.name.name]
+        new_context = context.create_child()
+        for i, arg in enumerate(args):
+            new_context.assign(ftn.decl.type.args.params[i].name, self.execute(ftn.decl.type.args.params[i].type, context), 1)
+            new_context[ftn.decl.type.args.params[i].name] = arg
+        return self.execute(ftn.body, new_context)
+    
+    def execute_Return(self, node: c_ast.Return, context: Context):
+        return self.evaluate(node.expr, context)
+
+    def evaluate(self, node, context: Context, left=False):
+        if isinstance(node, c_ast.ID):
+            if left:
+                return context.get_pointer(node.name)
+            else:
+                return context[node.name]
+        else:
+            return self.execute(node, context, left)
+
+    def execute(self, node, context: Context, left=False):
         node_name = node.__class__.__name__
         method_name = 'execute_' + node_name
         if hasattr(self, method_name):
-            return getattr(self, method_name)(node, context)
+            if left:
+                return getattr(self, method_name)(node, context, left)
+            else:
+                return getattr(self, method_name)(node, context)
         else:
             print('No method for', node_name)
             print(node)
@@ -258,6 +398,13 @@ class Executer:
     def run(self):
         context = Context()
         self.execute(self.ast, context)
+        
+        ftncall = c_ast.FuncCall(c_ast.ID('main'), c_ast.ExprList([]))
+        ret = self.execute(ftncall, context)
+        print(ret)
+
+        print(context)
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
--- test.c
+++ test.c
@@ -1,37 +1,14 @@
 
 #include <stdio.h>
 
-// char w;
-int s = 10;
-int k[3] = {1, 2, 3};
-// float j = 11.2;
-int *asd[3] = {&s, &s, &s};
-int *q = &s;
 
-
-int test(int* a, char b) {
-    float e[3] = {1.1, 2.2, 3.3};
-    e[1] = 2.3;
-    return (int)e[2];
-}
+int res = 0;
 
 int main() {
-    int a = 0;
-    int b = 1;
+    int a[5] = {1, 2, 3, 4, 5};
+    int *b = &a;
 
-    for (int i = 0; i < 2; i++) {
-        a += b;
-        int tmp = a;
-        a = b;
-        b = tmp;
-    }
-
-    test(&a, 'a');
-
-    printf("res = %d\n", a);
-
-    printf("Hello World!\n");
-    char* c = "hi";
+    *b = 10;
 
     return 0;
 }
